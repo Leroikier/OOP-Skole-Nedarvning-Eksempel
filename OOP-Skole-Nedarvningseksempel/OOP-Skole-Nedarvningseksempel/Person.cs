08f1b344bc4f92415aa41b7c1209402f78437959
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-29 14:06:58 +0200

    <Færdigt Resultat>

---
+++ OOP-Skole-Nedarvningseksempel/OOP-Skole-Nedarvningseksempel/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Skole_Nedarvningseksempel
+{
+    class Person
+    {
+        //Get set variabler
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        public string Adresse { get; set; }
+
+        //Default Constructor //Nødvendig
+        public Person()
+        {
+        }
+
+        public Person(string fnavn, string enavn, string adr)
+        {
+            Fornavn = fnavn;
+            Efternavn = enavn;
+            Adresse = adr;
+        }
+    }
+
+    class Personale : Person
+    {
+        public Personale()
+        {
+        }
+
+        public string LønKontoNr { get; set; }
+
+        public Personale(string fnavn, string enavn, string adr, string lkto) :  base (fnavn, enavn, adr)
+        {
+            LønKontoNr = lkto;
+        }
+    }
+
+    class Elev : Person
+    {
+        public Elev()
+        {
+        }
+
+        public int StudieNr { get; set; }
+
+        public Elev(string fnavn, string enavn, string adr, int stunr) : base(fnavn, enavn, adr)
+        {
+            StudieNr = stunr;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ OOP-Skole-Nedarvningseksempel/OOP-Skole-Nedarvningseksempel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Skole_Nedarvningseksempel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fornavn, Efternavn, Adresse. Nedarves fra Person Klassen kun LønKntoNr er unikt for Personale klassen
+            Personale p1 = new Personale { Fornavn = "Biver", Efternavn = "Jensen", Adresse = "Skælbæksgade", LønKontoNr = "123456789" };
+
+            //Fornavn, Efternavn, Adresse. Nedarves fra Person Klassen kun StudieNr er unikt for Elev klassen
+            Elev e1 = new Elev { Fornavn = "Jim", Efternavn = "Jones", Adresse = "Rockerborgen 4", StudieNr = 987654321 };
+
+            Console.WriteLine("Fornavn : " + p1.Fornavn + " | " + "Efternavn : " + p1.Efternavn + " | " + "Adresse : " + p1.Adresse + " | " + "Lønkonto nr : " + p1.LønKontoNr);
+
+            Console.WriteLine("Fornavn : " + e1.Fornavn + " | " + "Efternavn : " + e1.Efternavn + " | " + "Adresse : " + e1.Adresse + " | " + "Studie nr : " + e1.StudieNr);
+
+            Console.ReadKey();
+        }
+    }
+}
